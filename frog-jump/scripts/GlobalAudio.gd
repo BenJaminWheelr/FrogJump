7432ab7d6a2b4c35641bd3922cfda4f87d530f7b
Ben Wheeler <BenJaminWheelr@gmail.com>
2026-04-01 16:24:03 -0600

    audio fix

--- frog-jump/scripts/GlobalAudio.gd
+++ frog-jump/scripts/GlobalAudio.gd
@@ -1,12 +1,22 @@
 extends Node
 
-var audio_enabled: bool = false
+var audio_enabled: bool = false:
+	set(value):
+		audio_enabled = value
+		if audio_enabled:
+			_resume_bg()
+		else:
+			stop_all_bg()
+
+var current_music_stream: AudioStream
+var current_ambience_stream: AudioStream
+var current_music_volume: float = 0.0
+var current_ambience_volume: float = 0.0
 
 @onready var music_player = AudioStreamPlayer.new()
 @onready var ambience_player = AudioStreamPlayer.new()
 
 func _ready():
-	# Setup persistent players
 	add_child(music_player)
 	add_child(ambience_player)
 	
@@ -23,18 +33,38 @@ func play_sfx(effect: AudioStream):
 		player.finished.connect(player.queue_free)
 
 func play_music(stream: AudioStream, volume: float = 0.0):
+	current_music_stream = stream
+	current_music_volume = volume
+	
+	if not audio_enabled:
+		return
+		
 	if music_player.stream == stream and music_player.playing:
 		return
+		
 	music_player.stream = stream
 	music_player.volume_db = volume
 	music_player.play()
 
 func play_ambience(stream: AudioStream, volume: float = 0.0):
+	current_ambience_stream = stream
+	current_ambience_volume = volume
+	
+	if not audio_enabled:
+		return
+		
 	if ambience_player.stream == stream and ambience_player.playing:
 		return
+		
 	ambience_player.stream = stream
 	ambience_player.volume_db = volume
 	ambience_player.play()
+
+func _resume_bg():
+	if current_music_stream:
+		play_music(current_music_stream, current_music_volume)
+	if current_ambience_stream:
+		play_ambience(current_ambience_stream, current_ambience_volume)
 
 func stop_all_bg():
 	music_player.stop()
