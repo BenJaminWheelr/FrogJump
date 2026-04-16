7b44886783f57b850ec5567fe553f76ef32f8e79
Brody <brody@steidley.org>
2026-04-16 18:26:47 +0000

    Add audio to level class

--- frog-jump/scripts/level.gd
+++ frog-jump/scripts/level.gd
@@ -8,5 +8,15 @@ signal level_complete
 @export var bg_img2 : Texture2D = null;
 @export var bg_img3 : Texture2D = null;
 
+@export var music : AudioStream = null;
+@export var ambience : AudioStream = null;
+
+func _ready():
+	GlobalAudio.play_music(music);
+	GlobalAudio.play_ambience(ambience);
+
+func level_complete_anim_start() -> void:
+	level_clear_anim_started.emit();
+
 func level_completed() -> void:
-	level_complete.emit()
+	level_complete.emit();
