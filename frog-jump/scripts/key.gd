ef8e28427b45acc1023496962ef6fb61945a9d6f
Brody <brody@steidley.org>
2026-04-18 22:39:27 +0000

    Level 8 and tower 2 cutscene

--- frog-jump/scripts/key.gd
+++ frog-jump/scripts/key.gd
@@ -1,18 +1,20 @@
 extends Area2D
 
-const FOLLOW_SPEED = 8;
+const PLAYER_FOLLOW_SPEED = 8;
 
-var target : Player = null;
+@export var target : Node2D = null;
+@export var follow_speed = 8;
 
 func _ready():
 	$AnimatedSprite.play("spin");
 
 func _process(delta: float) -> void:
 	if target != null:
-		global_position = global_position.lerp(target.global_position, FOLLOW_SPEED * delta)
+		global_position = global_position.lerp(target.global_position, follow_speed * delta)
 
 
 func _on_area_entered(area: Area2D) -> void:
-	if area.get_parent() is Player and target == null:
+	if area.get_parent() is Player && (target == null || !target is Player):
 		target = area.get_parent();
 		area.get_parent().connect("lost_key", Callable(self, "queue_free"));
+		follow_speed = PLAYER_FOLLOW_SPEED;
