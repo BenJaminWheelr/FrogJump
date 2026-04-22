f8b63c522aedd5fd906b17d34fbbbe105b9c95aa
Ben Wheeler <BenJaminWheelr@gmail.com>
2026-04-21 18:09:14 -0600

    small fix to main menu

--- frog-jump/scripts/Global/GlobalState.gd
+++ frog-jump/scripts/Global/GlobalState.gd
@@ -6,42 +6,42 @@ var coin_count: int = 0
 
 
 func _ready() -> void:
-    _load_coins_from_save()
+	_load_coins_from_save()
 
 
 func _load_coins_from_save() -> void:
-    if SaveManager.data.has("player") and SaveManager.data["player"].has("coins"):
-        coin_count = int(SaveManager.data["player"]["coins"])
-    else:
-        coin_count = 0
+	if SaveManager.data.has("player") and SaveManager.data["player"].has("coins"):
+		coin_count = int(SaveManager.data["player"]["coins"])
+	else:
+		coin_count = 0
 
-    coin_count_changed.emit(coin_count)
+	coin_count_changed.emit(coin_count)
 
 
 func _persist_coins() -> void:
-    if not SaveManager.data.has("player"):
-        SaveManager.data["player"] = {}
+	if not SaveManager.data.has("player"):
+		SaveManager.data["player"] = {}
 
-    SaveManager.data["player"]["coins"] = coin_count
-    SaveManager.save_game()
+	SaveManager.data["player"]["coins"] = coin_count
+	SaveManager.save_game()
 
 
 func add_coin(amount: int = 1) -> void:
-    coin_count += amount
-    if coin_count < 0:
-        coin_count = 0
+	coin_count += amount
+	if coin_count < 0:
+		coin_count = 0
 
-    _persist_coins()
-    coin_count_changed.emit(coin_count)
+	_persist_coins()
+	coin_count_changed.emit(coin_count)
 
 
 func set_coins(value: int) -> void:
-    coin_count = max(value, 0)
-    _persist_coins()
-    coin_count_changed.emit(coin_count)
+	coin_count = max(value, 0)
+	_persist_coins()
+	coin_count_changed.emit(coin_count)
 
 
 func reset_coins() -> void:
-    coin_count = 0
-    _persist_coins()
-    coin_count_changed.emit(coin_count)
+	coin_count = 0
+	_persist_coins()
+	coin_count_changed.emit(coin_count)
--- frog-jump/scripts/Global/SaveManager.gd
+++ frog-jump/scripts/Global/SaveManager.gd
@@ -3,71 +3,71 @@ extends Node
 const SAVE_PATH = "user://savegame.json"
 
 var data = {
-    "player": {
-        "highest_level_unlocked": 1,
-        "coins": 0,
-    },
-    "settings": {
-        "audioEnabled": true,
-    },
-    "shop": {
-        "owned_items": [],
-        "equipped_item": "",
-    }
+	"player": {
+		"highest_level_unlocked": 1,
+		"coins": 0,
+	},
+	"settings": {
+		"audioEnabled": true,
+	},
+	"shop": {
+		"owned_items": [],
+		"equipped_item": "",
+	}
 }
 
 func _ready():
-    load_game()
-    _ensure_defaults()
+	load_game()
+	_ensure_defaults()
 
 
 func save_game():
-    var file = FileAccess.open(SAVE_PATH, FileAccess.WRITE)
-    if file:
-        var json_string = JSON.stringify(data)
-        file.store_string(json_string)
-        file.close()
+	var file = FileAccess.open(SAVE_PATH, FileAccess.WRITE)
+	if file:
+		var json_string = JSON.stringify(data)
+		file.store_string(json_string)
+		file.close()
 
 
 func update_data(section: String, key: String, value):
-    if data.has(section) and data[section].has(key):
-        data[section][key] = value
-        save_game()
+	if data.has(section) and data[section].has(key):
+		data[section][key] = value
+		save_game()
 
 
 func _ensure_defaults() -> void:
-    if not data.has("player"):
-        data["player"] = {"highest_level_unlocked": 1, "coins": 0}
-    if not data["player"].has("highest_level_unlocked"):
-        data["player"]["highest_level_unlocked"] = 1
-    if not data["player"].has("coins"):
-        data["player"]["coins"] = 0
+	if not data.has("player"):
+		data["player"] = {"highest_level_unlocked": 1, "coins": 0}
+	if not data["player"].has("highest_level_unlocked"):
+		data["player"]["highest_level_unlocked"] = 1
+	if not data["player"].has("coins"):
+		data["player"]["coins"] = 0
 
-    if not data.has("settings"):
-        data["settings"] = {"audioEnabled": true}
-    if not data["settings"].has("audioEnabled"):
-        data["settings"]["audioEnabled"] = true
+	if not data.has("settings"):
+		data["settings"] = {"audioEnabled": true}
+	if not data["settings"].has("audioEnabled"):
+		data["settings"]["audioEnabled"] = true
 
-    if not data.has("shop"):
-        data["shop"] = {}
-    if not data["shop"].has("owned_items"):
-        data["shop"]["owned_items"] = []
-    if not data["shop"].has("equipped_item"):
-        data["shop"]["equipped_item"] = ""
+	if not data.has("shop"):
+		data["shop"] = {}
+	if not data["shop"].has("owned_items"):
+		data["shop"]["owned_items"] = []
+	if not data["shop"].has("equipped_item"):
+		data["shop"]["equipped_item"] = ""
 
 
 func load_game():
-    if not FileAccess.file_exists(SAVE_PATH):
-        return
+	if not FileAccess.file_exists(SAVE_PATH):
+		return
 
-    var file = FileAccess.open(SAVE_PATH, FileAccess.READ)
-    if file:
-        var json_string = file.get_as_text()
-        file.close()
+	var file = FileAccess.open(SAVE_PATH, FileAccess.READ)
+	if file:
+		var json_string = file.get_as_text()
+		file.close()
 
-        var json = JSON.new()
-        var error = json.parse(json_string)
-        if error == OK:
-            data = json.data
-            print("Successfully loaded player data")
-            print(data)
+		var json = JSON.new()
+		var error = json.parse(json_string)
+		if error == OK:
+			data = json.data
+			print("Successfully loaded player data")
+			print(data)
